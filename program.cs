a450b98cb23abd5983953763200ea109118bd32f
RonaldOngom <ongomronald97@gmail.com>
2026-03-06 14:14:19 +0300

    ronald web fix

---
+++ program.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Prompt the user for the first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Prompt the user for the last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Display the formatted result
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+    }
+}
