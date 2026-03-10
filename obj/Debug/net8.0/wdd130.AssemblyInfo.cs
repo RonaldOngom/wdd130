302fc17b7454e57b178208537c5ce7ce17f755d2
RonaldOngom <ongomronald97@gmail.com>
2026-03-11 00:11:39 +0300

    scooter

--- obj/Debug/net8.0/wdd130.AssemblyInfo.cs
+++ obj/Debug/net8.0/wdd130.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("wdd130")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfb5f620f4242552dbbdc23c61e6aed23b7aa778")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3df5c206458d74a743ef9a4bad02d6229c76993a")]
 [assembly: System.Reflection.AssemblyProductAttribute("wdd130")]
 [assembly: System.Reflection.AssemblyTitleAttribute("wdd130")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
